b3d57d34bdcc9e93fc32fe604d1dba54e3e16b09
Emam AbdullAziz <eng.waheedea@gmail.com>
2026-06-14 03:33:51 +0300

    style: increase hero student image size (12→16rem mobile, 24→28rem desktop) + orbit radius scaled up

--- components/HeroWithHalo.js
+++ components/HeroWithHalo.js
@@ -19,13 +19,13 @@ export default function HeroWithHalo() {
       </div>
 
       <div className="orbit-inner" aria-hidden="true">
-        <div className="orb spin" style={{ ['--r']: '140px', ['--size']: '40px', ['--d']: '10s' }}>
+        <div className="orb spin" style={{ ['--r']: '170px', ['--size']: '40px', ['--d']: '10s' }}>
           <div className="material-symbols-outlined" style={{fontSize: '24px', color: '#FFDD57'}}>rocket_launch</div>
         </div>
-        <div className="orb spin-rev" style={{ ['--r']: '120px', ['--size']: '36px', ['--d']: '7s' }}>
+        <div className="orb spin-rev" style={{ ['--r']: '150px', ['--size']: '36px', ['--d']: '7s' }}>
           <div className="material-symbols-outlined" style={{fontSize: '22px', color: '#FFDD57'}}>smart_toy</div>
         </div>
-        <div className="orb spin" style={{ ['--r']: '160px', ['--size']: '32px', ['--d']: '13s' }}>
+        <div className="orb spin" style={{ ['--r']: '190px', ['--size']: '32px', ['--d']: '13s' }}>
           <div className="material-symbols-outlined" style={{fontSize: '20px', color: '#FFDD57'}}>science</div>
         </div>
       </div>
